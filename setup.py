42177a5400f615b09befec140b5e1667c09a4112
Mirwais Farahi <waisfarahi@example.com>
2024-06-19 17:09:22 +0430

    Update File

--- setup.py
+++ setup.py
@@ -9,4 +9,17 @@ setup(
         'pandas',
         'koboextractor',
     ],
+
+    author='Mirwais Farahi',
+    author_email='waisfarahi@example.com',
+    description='Python module for automating data download from KoboToolbox into pandas DataFrames.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/mirwais-farahi/KoboDataExtractor',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+
 )
